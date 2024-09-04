7a2ca9dfb4dc124564fc38ddc77229ecfee1964a
andrewcgaitskell <andrew@gaitskell.com>
2024-09-04 10:11:55 +0100

    Update setenv.sh

--- podman/setenv.sh
+++ podman/setenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Show env vars
-grep -v '^#' .env
+grep -v '^#' .gitaction_env
 
 # Export env vars
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' .gitaction_env | xargs)
